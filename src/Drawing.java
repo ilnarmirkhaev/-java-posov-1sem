6ecdf62cbb2d26193f81217d53c4ee3e8cb06a88
Ilnar Mirkhaev <st076505@student.spbu.ru>
2020-06-11 01:04:10 +0300

    Добавил в Drawing методы drawCircle и draw. И порисовал немного

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Drawing {
     // NON-STATIC
     // private fields
@@ -16,13 +18,19 @@ public class Drawing {
             for (int j = 0; j < w; j++)
                 this.d[j][i] = c;
         }
+        System.out.println("Изображение создано. Помните, что координаты начинаются с 0.");
     }
 
     // public methods
+
+    public char[][] getD() {
+        return d;
+    }
+
     public void print() {
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++)
-                System.out.print(d[j][i]);
+                System.out.print(d[j][i] + " ");
             System.out.println();
         }
     }
@@ -56,10 +64,55 @@ public class Drawing {
             System.out.println("Введите корректные координаты.");
     }
 
-    public void drawRectangle(int column1, int row1, int column2, int row2, char c) {
-        drawHorizontalLine(row1, column1, column2, c);
-        drawHorizontalLine(row2, column1, column2, c);
-        drawVerticalLine(column1, row1, row2, c);
-        drawVerticalLine(column2, row1, row2, c);
+    public void drawRectangle(int column1, int row1, int column2, int row2, char c, boolean fill) {
+        if (fill) {
+            int a = Math.min(column1, column2);
+            int b = Math.max(column1, column2);
+            for (int i = a; i <= b; i++)
+                drawVerticalLine(i, row1, row2, c);
+        }
+        else {
+            drawHorizontalLine(row1, column1, column2, c);
+            drawHorizontalLine(row2, column1, column2, c);
+            drawVerticalLine(column1, row1, row2, c);
+            drawVerticalLine(column2, row1, row2, c);
+        }
+    }
+
+    public void drawCircle(int centerX, int centerY, double radius, char c) {
+        if (centerX >= 0 && centerX < w && centerY >= 0 && centerY < h) {
+            if (radius > 0) {
+                for (int i = 0; i < w; i++)
+                    for (int j = 0; j < h; j++)
+                        if (distance(centerX, centerY, i, j) <= radius)
+                            d[i][j] = c;
+            }
+            else if (radius == 0)
+                setPoint(centerX, centerY, c);
+            else
+                System.out.println("Введите неотрицательный радиус.");
+        }
+        else
+            System.out.println("Введите корректные координаты.");
+
+    }
+
+    public void draw(int x, int y, Drawing d) {
+        if (x >= 0 && x < w && y >= 0 && y < h) {
+            char[][] pic = d.getD();
+            for (int i = 0; i < d.w; i++) {
+                for (int j = 0; j < d.h; j++)
+                    if (x + i < w && y + j < h)
+                        this.d[x + i][y + j] = pic[i][j];
+            }
+        }
+        else
+            System.out.println("Введите корректные координаты.");
+
+    }
+
+    // private methods
+    private double distance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 }
--- src/ExperimentsWithDrawing.java
+++ src/ExperimentsWithDrawing.java
@@ -1,18 +1,42 @@
 public class ExperimentsWithDrawing {
     public static void main(String[] args) {
-        Drawing d = new Drawing(7, 4, '.');
-        d.print();
-        System.out.println("----------");
-        d.setPoint(4, 2, '*');
-        d.print();
-        System.out.println("----------");
-        d.drawVerticalLine(6, 1, 3, '#');
-        d.print();
-        System.out.println("----------");
-        d.drawHorizontalLine(1, 3, 6, '@');
-        d.print();
-        System.out.println("----------");
-        d.drawRectangle(0, 1, 2, 3, '$');
-        d.print();
+        Drawing d1 = new Drawing(8, 8, '_');
+        d1.print();
+        System.out.println("---------------");
+        d1.setPoint(4, 2, '*');
+        d1.print();
+        System.out.println("---------------");
+        d1.drawVerticalLine(6, 1, 3, '#');
+        d1.print();
+        System.out.println("---------------");
+        d1.drawHorizontalLine(1, 3, 6, '@');
+        d1.print();
+        System.out.println("---------------");
+        d1.drawRectangle(1, 4, 5, 6, '$', true);
+        d1.print();
+        System.out.println("---------------");
+        d1.drawRectangle(0, 2, 4, 7, '?', false);
+        d1.print();
+        System.out.println("---------------");
+
+        Drawing d2 = new Drawing(21, 21, '.');
+        d2.drawCircle(10, 10, 9.5, '*');
+        d2.print();
+        System.out.println("---------------");
+        d2.drawCircle(6, 7, 2.5, ' ');
+        d2.drawCircle(14, 7, 2.5, ' ');
+        d2.drawCircle(6, 7, 0, '#');
+        d2.drawCircle(14, 7, 0, '#');
+        d2.print();
+        System.out.println("---------------");
+        d2.draw(5, 5, d1);
+        d2.print();
+        System.out.println("---------------");
+        d2.draw(15, 15, d1);
+        d2.print();
+
+//        System.out.println("---------------");
+//        Drawing gioconda = new Drawing(40, 40, '.');
+//        gioconda.print();
     }
 }
