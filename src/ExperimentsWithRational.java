898e97d3ec6af588c89d6578d5b43a711ed2e37f
Ilnar Mirkhaev <st076505@student.spbu.ru>
2020-06-10 19:01:01 +0300

    Разобрался с арифметическими действиями над дробями. Добавил метод private void simplify для упрощения дробей.

--- src/ExperimentsWithRational.java
+++ src/ExperimentsWithRational.java
@@ -1,12 +1,35 @@
 public class ExperimentsWithRational {
+
     public static void main(String[] args) {
-        Rational r1 = new Rational(3, 7);
-        Rational r2 = new Rational(10,25);
+        Rational r1 = new Rational(1, 6);
+        Rational r2 = new Rational(2, 6);
+
         System.out.println(r1.toString());
         System.out.println(r2.toString());
         System.out.println(r2.toDouble());
+
+        System.out.println("----------------");
+
         r1.addInPlace(r2);
         System.out.println(r1.toString());
         System.out.println(r2.toString());
+
+        System.out.println("----------------");
+
+        Rational r3 = new Rational(4);
+        System.out.println(r3.toString());
+        r3.subInPlace(r1);
+        System.out.println(r3.toString());
+
+        System.out.println("----------------");
+
+        Rational r4 = r3.add(r2);
+        System.out.println(r4.toString());
+        System.out.println(r4.toDouble());
+
+        System.out.println("----------------");
+
+
     }
+
 }
--- src/Rational.java
+++ src/Rational.java
@@ -1,23 +1,64 @@
 public class Rational {
-    private int n = 0;
+
+    // NON-STATIC
+    // private fields
+    private int n;
     private int d = 1;
 
+    // public constructors
     public Rational(int n, int d) {
-        if (d > 0) {
-            int c = gcd(n, d);
-            this.n = n / c;
-            this.d = d / c;
-        }
-        else {
-            this.n = n;
+        this.n = n;
+        if (d > 0)
+            this.d = d;
+        else
             System.out.println("Warning!\nЗнаменатель заменен на 1. Введите положительный знаменатель.");
-        }
+        simplify();
     }
 
     public Rational(int n) {
         this.n = n;
     }
 
+    public Rational add(Rational r) {
+        int a = this.n * r.d + r.n * this.d;
+        int b = this.d * r.d;
+        Rational result = new Rational(a, b);
+        result.simplify();
+        return result;
+    }
+
+    public Rational sub(Rational r) {
+        int a = this.n * r.d - r.n * this.d;
+        int b = this.d * r.d;
+        Rational result = new Rational(a, b);
+        result.simplify();
+        return result;
+    }
+
+    public Rational mul(Rational r) {
+        int a = this.n * r.n;
+        int b = this.d * r.d;
+        Rational result = new Rational(a, b);
+        result.simplify();
+        return result;
+    }
+
+    public Rational div(Rational r) {
+        if (r.n != 0) {
+            int a = this.n * r.d;
+            int b = this.d * r.n;
+            Rational result = new Rational(a, b);
+            result.simplify();
+            return result;
+        }
+        else {
+            System.out.println("Деление на 0! Измените числитель делителя.");
+            System.out.println("Новая дробь равна 0.");
+            return new Rational(0);
+        }
+    }
+
+    // public methods
     public String toString() {
         if (n == 0)
             return "0";
@@ -27,20 +68,47 @@ public class Rational {
     }
 
     public double toDouble() {
-        double num = n;
-        return num/d;
+        return (double) n / d;
     }
 
-    private static int gcd(int a, int b) {
+    public void addInPlace(Rational r) {
+        this.n = this.n * r.d + r.n * this.d;
+        this.d = this.d * r.d;
+        simplify();
+    }
+
+    public void subInPlace(Rational r) {
+        this.n = this.n * r.d - r.n * this.d;
+        this.d = this.d * r.d;
+        simplify();
+    }
+
+    public void mulInPlace(Rational r) {
+        this.n = this.n * r.n;
+        this.d = this.d * r.d;
+        simplify();
+    }
+
+    public void divInPlace(Rational r) {
+        if (r.n != 0) {
+            this.n = this.n * r.d;
+            this.d = this.d * r.n;
+            simplify();
+        }
+        else
+            System.out.println("Деление на 0! Измените числитель делителя.");
+    }
+
+    // private methods
+    private int gcd(int a, int b) {
         if (b == 0)
             return Math.abs(a);
         return gcd(b, a % b);
     }
 
-    public void addInPlace(Rational r) {
-        int c = gcd(this.d, r.d);
-        this.n = (this.n * r.d + r.n * this.d) / c;
-        this.d = this.d * r.d / c;
+    private void simplify() {
+        int c = gcd(this.n, this.d);
+        this.n /= c;
+        this.d /= c;
     }
-
 }
