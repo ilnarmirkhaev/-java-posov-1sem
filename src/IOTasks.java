2ba4c850cbd7287982ff2dfdbea1a418efa15531
Ilnar Mirkhaev <st076505@student.spbu.ru>
2020-06-11 13:40:35 +0300

    Файлы в java это тяжело...

--- src/IOTasks.java
+++ src/IOTasks.java
@@ -1,23 +1,24 @@
-import java.nio.file.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.File;
 import java.util.Scanner;
 
 public class IOTasks {
-//    public static void main(String[] args) {
-//        System.out.println(fileIntegersSum("D:\\JavaProjects\\ints.txt"));
-//    }
-//
-//    private static int fileIntegersSum(String s) throws Exception {
-//        int answer = 0;
-//        Path filepath = Paths.get(s);
-//        try (Scanner in = new Scanner(filepath)) {
-//            while (in.hasNext()) {
-//                if (in.hasNextInt())
-//                    answer += in.nextInt();
-//                else in.next();
-//            }
-//        }
-//        return answer;
-//    }
+    public static void main(String[] args) throws Exception{
+        Path f = Paths.get("a.txt");
+        System.out.println(f.toAbsolutePath());
+        fileIntSum(f);
+    }
+
+    private static void fileIntSum(Path f) throws Exception {
+        int sum = 0;
+        try (Scanner in = new Scanner(f)) {
+            while (in.hasNext()) {
+                if (in.hasNextInt())
+                    sum += in.nextInt();
+                else in.next();
+            }
+        } System.out.println(sum);
+    }
 
 }
