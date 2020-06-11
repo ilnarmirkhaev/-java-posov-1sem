8be6d2dd1797c6a1b96af371ebc461889ed25a0a
Ilnar Mirkhaev <st076505@student.spbu.ru>
2020-06-11 13:54:45 +0300

    Сделал запись строк в файл.

--- src/IOTasks.java
+++ src/IOTasks.java
@@ -1,16 +1,18 @@
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.io.*;
 import java.util.Scanner;
 
 public class IOTasks {
     public static void main(String[] args) throws Exception{
-        Path f = Paths.get("a.txt");
-        System.out.println(f.toAbsolutePath());
+        String[] lines = {"Какой-то текст", "Ещё текст!", "Some text in English", ":)"};
+        File f = new File("a.txt");
+//        System.out.println(f.toAbsolutePath());
         fileIntSum(f);
+        writeLinesToFile(lines, new File("b.txt"));
     }
 
-    private static void fileIntSum(Path f) throws Exception {
+    private static void fileIntSum(File f) throws Exception {
         int sum = 0;
         try (Scanner in = new Scanner(f)) {
             while (in.hasNext()) {
@@ -19,6 +21,13 @@ public class IOTasks {
                 else in.next();
             }
         } System.out.println(sum);
+    }
+
+    private static void writeLinesToFile(String[] lines, File f)  throws Exception{
+        PrintStream ps = new PrintStream(f, StandardCharsets.UTF_8);
+        for (String line : lines)
+            ps.println(line);
+        System.out.println("Строки записаны в файл");
     }
 
 }
